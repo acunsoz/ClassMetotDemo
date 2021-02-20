20e8a12825633470d4ac332ef315ac9f503a9e2d
ALIHAN <ALIHAN@DESKTOP-52972LB>
2021-02-20 15:28:42 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi: "+musteri.Adi);
+            Console.WriteLine("-------------");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi: "+musteri.Adi );
+            Console.WriteLine("-------------");
+        }
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Mehmet";
+            musteri1.Soyadi = "Cam";
+            musteri1.id =56894;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Ahmet";
+            musteri2.Soyadi = "Tat";
+            musteri2.id = 56823;
+            
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Kadir";
+            musteri3.Soyadi = "Tik";
+            musteri3.id = 56846;
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Adi = "Merve";
+            musteri4.Soyadi = "Dam";
+            musteri4.id = 56872;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3,musteri4 };
+            
+            foreach (var musterin in musteriler)
+            {
+                Console.WriteLine("Musterilerin Kimligi\n");
+                Console.WriteLine("Musteri adi: "+musterin.Adi);
+                Console.WriteLine("Musteri soyadi: "+musterin.Soyadi);
+                Console.WriteLine("Musteri id: "+musterin.id);
+                Console.WriteLine("-------------");
+
+            }
+
+
+            MusteriManager islem = new MusteriManager();
+            
+
+            islem.Ekle(musteri4);
+            islem.Ekle(musteri2);
+
+            islem.Sil(musteri3);
+            islem.Sil(musteri1);
+
+
+        }
+    }
+}
